a30661541c63d89f70f655c42dded57ccab831bb
Pier-Luc Nadeau <pierrotnad@hotmail.ca>
2020-03-09 15:05:07 -0400

    Création de table SQL terminer

--- Sql/Creation_bd.sql
+++ Sql/Creation_bd.sql
@@ -1,31 +1,191 @@
 USE [master]
 GO
 
-CREATE DATABASE GestionStage
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'GestionStage')
+BEGIN
+	CREATE DATABASE GestionStage 
+END
+GO
 
 use GestionStage
+GO
 
 CREATE TABLE MilieuStage (
-	IDMilieuStage INT PRIMARY KEY,
-	Titre VARCHAR,
-	[Description] VARCHAR,
-	NoCivique VARCHAR,
-	Rue VARCHAR,
-	CodePostal VARCHAR,
-	Province VARCHAR,
-	Pays VARCHAR,
-	NoTelephone VARCHAR,
+	IDMilieuStage INT IDENTITY PRIMARY KEY,
+	Titre VARCHAR(100),
+	[Description] VARCHAR(1000),
+	NoCivique VARCHAR(10),
+	Rue VARCHAR(100),
+	CodePostal VARCHAR(7),
+	Ville VARCHAR(100),
+	Province VARCHAR(100),
+	Pays VARCHAR(100),
+	NoTelephone VARCHAR(20),
 	Etat BIT,
 	DateHeureCreation DATETIME,
 	DateHeureModification DATETIME
 )
 GO
 
-
 CREATE TABLE Stage (
-	IDStage INT,
+	IDStage INT IDENTITY PRIMARY KEY,
 	IDMilieuStage INT,
+	Titre VARCHAR(100),
+	[Description] VARCHAR(1000),
+	NbPostes INT,
+	Statut TINYINT,
+	PeriodeTravail TINYINT,
+	NbHeureSemaine INT,
+	DateDebut DATETIME,
+	DateFin DATETIME,
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDMilieuStage) REFERENCES MilieuStage(IDMilieuStage) 
+)
+GO
+
+CREATE TABLE Restriction (
+	IDRestriction INT IDENTITY PRIMARY KEY,
+	Titre VARCHAR(100),
+	[Description] VARCHAR(1000),
+	Etat BIT,
 	DateHeureCreation DATETIME,
 	DateHeureModification DATETIME
 )
 GO
+
+CREATE TABLE MilieuStageRestriction (
+	IDMilieuStageRestriction INT IDENTITY PRIMARY KEY,
+	IDRestriction INT,
+	IDMilieuStage INT,
+	Titre VARCHAR(100),
+	[Description] VARCHAR(1000),
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDMilieuStage) REFERENCES MilieuStage(IDMilieuStage),
+	FOREIGN KEY (IDRestriction) REFERENCES Restriction(IDRestriction)
+)
+GO
+
+CREATE TABLE StageRestriction (
+	IDStageRestriction INT IDENTITY PRIMARY KEY,
+	IDStage INT,
+	IDRestriction INT,
+	Titre VARCHAR(100),
+	[Description] VARCHAR(1000),
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDStage) REFERENCES Stage(IDStage),
+	FOREIGN KEY (IDRestriction) REFERENCES Restriction(IDRestriction)
+)
+GO
+
+CREATE TABLE PersonneContact (
+	IDPersonneContact INT IDENTITY PRIMARY KEY,
+	Prenom VARCHAR(50),
+	Nom VARCHAR(50),
+	Fonction VARCHAR(100),
+	NoTelephone VARCHAR(20),
+	Courriel VARCHAR(50),
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME
+)
+GO
+
+CREATE TABLE PersonneContactMilieuStage (
+	IDPersonneContactMilieuStage INT IDENTITY PRIMARY KEY,
+	IDPersonneContact INT,
+	IDMilieuStage INT,
+	Prenom VARCHAR(50),
+	Nom VARCHAR(50),
+	Fonction VARCHAR(100),
+	NoTelephone VARCHAR(20),
+	Courriel VARCHAR(50),
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDPersonneContact) REFERENCES PersonneContact(IDPersonneContact),
+	FOREIGN KEY (IDMilieuStage) REFERENCES MilieuStage(IDMilieuStage)
+)
+GO
+
+CREATE TABLE PersonneContactStage (
+	IDPersonneContactStage INT IDENTITY PRIMARY KEY,
+	IDPersonneContact INT,
+	IDStage INT,
+	Prenom VARCHAR(50),
+	Nom VARCHAR(50),
+	Fonction VARCHAR(100),
+	NoTelephone VARCHAR(20),
+	Courriel VARCHAR(50),
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDPersonneContact) REFERENCES PersonneContact(IDPersonneContact),
+	FOREIGN KEY (IDStage) REFERENCES Stage(IDStage)
+)
+GO
+
+CREATE TABLE Programme (
+	IDProgramme INT IDENTITY PRIMARY KEY,
+	Nom VARCHAR(100),
+	Sigle VARCHAR(15),
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME
+)
+GO
+
+CREATE TABLE ProgrammeStage (
+	IDProgrammeStage INT IDENTITY PRIMARY KEY,
+	IDProgramme INT,
+	IDStage INT,
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDProgramme) REFERENCES Programme(IDProgramme),
+	FOREIGN KEY (IDStage) REFERENCES Stage(IDStage)
+)
+GO
+
+CREATE TABLE Etudiant (
+	IDEtudiant INT IDENTITY PRIMARY KEY,
+	IDProgramme INT,
+	NoDA INT,
+	Prenom VARCHAR(100),
+	Nom VARCHAR(100),
+	Courriel VARCHAR(100),
+	Photo VARBINARY(8000),
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME
+)
+
+GO
+
+CREATE TABLE StageEtudiant (
+	IDStageEtudiant INT IDENTITY PRIMARY KEY,
+	IDStage INT,
+	IDEtudiant INT,
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDStage) REFERENCES Stage(IDStage),
+	FOREIGN KEY (IDEtudiant) REFERENCES Etudiant(IDEtudiant)
+)
+
+CREATE TABLE ChoixStageEtudiant (
+	IDChoixStageEtudiant INT IDENTITY PRIMARY KEY,
+	IDStage INT,
+	IDEtudiant INT,
+	Ordre TINYINT,
+	Etat BIT,
+	DateHeureCreation DATETIME,
+	DateHeureModification DATETIME,
+	FOREIGN KEY (IDStage) REFERENCES Stage(IDStage),
+	FOREIGN KEY (IDEtudiant) REFERENCES Etudiant(IDEtudiant)
+)
